d73404ba27326f3a9204f9d8cf3b06f9341b49f9
Jeff Wilson <whackscript@gmail.com>
2012-05-31 13:23:34 -0500

    Initial Commit

---
+++ SimpleAudioTrigger1/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SimpleAudioTrigger1
+//
+//  Created by frogTemplate on 5/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
